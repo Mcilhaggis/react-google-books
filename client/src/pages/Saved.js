795d3ce015bdd84a71a62822e1771812b743c838
Rachael McIlhagga <rachael.mcilhagga@live.co.uk>
2021-03-16 15:08:20 -0400

    adding get route

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,4 +1,5 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import BookResult from '../components/BookResult/BookResult';
 import API from "../utils/API";
 
 
@@ -14,16 +15,27 @@ function Saved() {
     //function to call the api to get all the saved books
     function loadBooks() {
         API.getBooks()
-        .then(res => 
-            setBooks(res.data)
+            .then(res =>
+                // console.log(res.data)
+                setBooks(res.data)
             )
             .catch(err => console.log(err))
     }
 
-
+console.log(books)
     return (
         <>
-        
+    {/* {books.map(book => {
+        <BookResult
+        key={book.volumeInfo.title}
+        title={book.volumeInfo.title}
+        subtitle={book.volumeInfo.subtitle}
+        authors={book.volumeInfo.authors}
+        description={book.volumeInfo.description}
+        image={book.volumeInfo.imageLinks !== undefined ? book.volumeInfo.imageLinks.thumbnail : ''}
+        alt={book.volumeInfo.title}
+        />
+    })} */}
         </>
     )
 }
