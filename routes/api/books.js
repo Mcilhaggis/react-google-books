21d73253a6ae65db0791964e3bafa3bc8b86fccc
Rachael McIlhagga <rachael.mcilhagga@live.co.uk>
2021-03-16 00:13:51 -0400

    £+

--- routes/api/books.js
+++ routes/api/books.js
@@ -9,8 +9,8 @@ router.route("/")
 // Matches with "/api/books/:id"
 router
   .route("/:id")
-  .get(booksController.findById)
-  .put(booksController.update)
+  // .get(booksController.findById)
   .delete(booksController.remove);
+
 
 module.exports = router;
