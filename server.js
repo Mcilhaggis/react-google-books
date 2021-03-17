ff707fc941484be33be9c6a8b02e000e8ed765a6
Rachael McIlhagga <rachael.mcilhagga@live.co.uk>
2021-03-16 21:12:00 -0400

    server edit

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require("mongoose");
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -36,5 +37,4 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
  
  app.listen(PORT, () => {
    console.log(`🌎 ==> API server now on port ${PORT}!`);
-   console.log(mongoose.connection.readyState);
 });
